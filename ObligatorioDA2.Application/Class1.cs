48816c1ae25702f955d253206ba65fd6b9550fac
Guti <gutierrez.matias@outlook.com>
2021-08-07 21:12:12 -0300

    create and reference projects

---
+++ ObligatorioDA2.Application/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ObligatorioDA2.Application
+{
+    public class Class1
+    {
+    }
+}
---
+++ ObligatorioDA2.EntityFrameworkCore/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ObligatorioDA2.EntityFrameworkCore
+{
+    public class Class1
+    {
+    }
+}
